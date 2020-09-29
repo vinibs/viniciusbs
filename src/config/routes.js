3033b068db477fbc96f4c6011947763555f4a57a
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2020-09-29 09:54:12 -0300

    Remove os arquivos da página de aniversário

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,15 +6,10 @@ import '../view/index.js'
 import '../view/pages/about.js'
 import '../view/pages/projects.js'
 import '../view/errors/not-found.js'
-import '../view/pages/aniversario-vic.js'
 
 route.set('/', 'app-index')
 route.set('/about-me', 'about-me')
 route.set('/projects', 'my-projects')
-
-const date = new Date()
-if (date.getFullYear() === 2020 && date.getMonth() + 1 === 9 && date.getDate() >= 24 && date.getDate() <= 26)
-    route.set('/feliz-aniversario', 'aniversario-vic')
 
 // Wildcard route for anything not listed above
 route.set('*', 'not-found')
