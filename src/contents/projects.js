bf6a6f9b62f931f62cf369cf2834dbb80761ffa5
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2023-03-27 18:33:41 -0300

    feat: add rabbitMQ tag on metrics project

--- src/contents/projects.js
+++ src/contents/projects.js
@@ -62,6 +62,7 @@ export const projects = {
             "Node JS", 
             "TypeScript",
             "React",
+            "RabbitMQ",
             "Jira API",
             "Apache Superset",
             "Docker",
