cc4619055941137f8a724c7310d2fe2b1c15a84f
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2026-06-14 11:39:02 +0100

    feat: add support for active nav items on navbar

--- src/components/main-menu/index.js
+++ src/components/main-menu/index.js
@@ -2,10 +2,40 @@ import { LiraElement } from '/js/lira.js'
 import { navItems } from '../../contents/nav.js'
 
 export class MainMenu extends LiraElement {
+    currentHash = null
+
     constructor () {
         super(true)
         this.useStyle('./styles.css')
+
+        this.currentHash = window.location.hash
+
+        window.navigation.addEventListener("navigate", (event) => {
+            this.clearActiveMenuItem()
+            this.setActiveMenu()
+        })
     }
+
+    clearActiveMenuItem () {
+        const activeItems = this.shadowRoot.querySelectorAll(".active")
+
+        for (const item of activeItems) {
+            item.classList.remove("active")
+        }
+    }
+
+
+    setActiveMenu () {
+        this.currentHash = window.location.hash
+
+        for (const navItem of navItems) {
+            if (this.currentHash.includes(navItem.href)) {
+                const navElement = this.shadowRoot.querySelector(`[href='${navItem.href}']`)
+                navElement.classList.add("active")
+            }
+        }
+    }
+
 
     render () {
         return `
@@ -15,7 +45,8 @@ export class MainMenu extends LiraElement {
                         return `
                         <a href="${navItem.href}"
                             title="${navItem.title}"
-                            aria-label="${navItem.title}">
+                            aria-label="${navItem.title}"
+                            ${this.currentHash.includes(navItem.href) ? 'class="active"' : '' }>
                             ${navItem.title}
                         </a>
                         `
