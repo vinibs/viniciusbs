964e83c83211a618e9bdb07b67abc4a1dbf071ea
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2026-06-15 18:21:05 +0100

    refactor: rephrase about text

--- src/contents/about.js
+++ src/contents/about.js
@@ -24,7 +24,7 @@ export const personalInfo = {
 
         I'm a Software Engineer focused on <strong>Quality, Patterns and Governance</strong>, with professional experience since 2019, and who sees mentorship, engineering standards and quality as <strong>a single unit</strong> and not as three separate forces.
 
-        I ensure consistency and longevity of software projects by thoroughly reviewing code, consistently mentoring (and learning from) peers and automating repetitive workflows, simplifying them where possible, for the sake of <strong>process quality</strong>. I understand "quality" is a complex concept, shaped by the intersection of code, product and process attributes – measured by <strong>assessing the real problem</strong> and how the proposed solution covers it.
+        I ensure consistency and longevity of software projects by thoroughly reviewing code, consistently mentoring (and learning from) peers and automating repetitive workflows, simplifying them where possible, for the sake of <strong>process quality</strong>. I understand "quality" is a complex concept, shaped by the intersection of code, product and process attributes – measured by <strong>assessing the real problem</strong> and how it is covered by the proposed solution.
 
         My current focus is on merging core backend logic with mentoring, quality (in all contexts) and <strong>software longevity</strong>, ensuring each problem is actually understood and solved, with a solution that is <strong>built to last</strong> and easy to maintain over the long-term.
     `,
